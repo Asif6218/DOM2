aa283b0f4222802139c00be16557c9ee079efd5a
Asif6218 <mohdasif6227@gmail.com>
2021-08-21 22:51:19 -0600

    counter

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
+let rootEl=document.getElementById("root");
+
+let count=document.createElement("h1");
+count.innerHTML=0;
+rootEl.appendChild(count);
+
+let incrementbn=document.createElement("button");
+incrementbn.setAttribute("onclick","increment()");
+incrementbn.innerText="increment"
+rootEl.appendChild(incrementbn);
+
+let decrementbn=document.createElement("button");
+decrementbn.setAttribute("onclick","decrement()");
+decrementbn.innerText="decrement"
+rootEl.appendChild(decrementbn);
+
+let resetbn=document.createElement("button");
+resetbn.setAttribute("onclick","reset()");
+resetbn.innerText="reset"
+rootEl.appendChild(resetbn);
+
+
+
+
+
 function increment(){
-    let count1=document.getElementById("counter");
-    let currentcount =count1.innerText;
+    // let count1=document.getElementById("counter");
+    let currentcount =count.innerText;
     let updatedcount = parseInt(currentcount)+1;
-    count1.innerHTML=updatedcount;
+    count.innerHTML=updatedcount;
 }
 function decrement(){
-    let count1=document.getElementById("counter");
-    let currentcount =count1.innerText;
+    // let count1=document.getElementById("counter");
+    let currentcount =count.innerText;
     let updatedcount = parseInt(currentcount)-1;
-    count1.innerHTML=updatedcount;
+    count.innerHTML=updatedcount;
 }
 function reset(){
-    let count1=document.getElementById("counter");
-    count1.innerHTML=0;
+    // let count1=document.getElementById("counter");
+    count.innerHTML=0;
 }
